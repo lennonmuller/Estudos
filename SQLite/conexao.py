49233cef305d27c7c3a2eca34274056be0097b69
Lennon Muller <lennon-muller@hotmail.com>
2024-11-30 14:24:27 +0000

    SQLite

---
+++ SQLite/conexao.py
@@ -0,0 +1,30 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH/'meu_banco.sqlite')
+cursor = conexao.cursor()
+
+def criar_tabela(conexao, cursor):
+    cursor.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))")
+    conexao.commit()
+
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute(f"INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+    conexao.commit()
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
+    conexao.commit()
+
+def excluir_registro(conexao, cursor, id):
+    data = (id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?;", data)
+    conexao.commit()
+
+#inserir_registro(conexao, cursor, 'Bob', 'boobsbob@boob.com')
+#atualizar_registro(conexao, cursor, 'Lennon Müler', 'lennon-muller@hotmail.com', 1)
+excluir_registro(conexao, cursor, 3)
